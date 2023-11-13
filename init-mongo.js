715fab4a60fb23e2007ecf83ba21e8cf0f3cee9e
Emilio Laurence Dimalanta <2201851@slu.edu.ph>
2023-11-14 00:02:51 +0800

    add tasks collection in Dockerfile

--- init-mongo.js
+++ init-mongo.js
@@ -12,10 +12,43 @@ db.createUser({
   roles: ["readWrite"],
 });
 
+// For Debug
+
 db.createCollection("registeredUsers");
 
 db.registeredUsers.insertMany([
   { name: "John Doe", age: 25, email: "john@example.com" },
   { name: "Jane Doe", age: 30, email: "jane@example.com" },
-  // Add more dummy data as needed
+]);
+
+db.createCollection("tasks");
+
+db.tasks.insertMany([
+  {
+    title: "Finish Filter Groups Page",
+    description: "Create a page for filtering using groups",
+    status: "unfinished",
+    group: "",
+    assignedTo: {
+      $oid: "6550da34a2e08ab6e5f80147",
+    }
+  },
+  {
+    title: "Create Home Page in Figma",
+    description: "Start home page prototype in Figma",
+    status: "unfinished",
+    group: "",
+    assignedTo: {
+      $oid: "6550da34a2e08ab6e5f80147",
+    }
+  },
+  {
+    title: "Meeting w/ Lorens",
+    description: "Discuss about app Google Meet Link: gugle.kom",
+    status: "finished",
+    group: "",
+    assignedTo: {
+      $oid: "6550da34a2e08ab6e5f80147",
+    }
+  },
 ]);
