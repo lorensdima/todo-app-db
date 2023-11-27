598b62b653ed8871acb029c8179ba8d465b22df2
Emilio Laurence Dimalanta <2201851@slu.edu.ph>
2023-11-27 22:32:46 +0800

    edit object id instaciation for test data

--- init-mongo.js
+++ init-mongo.js
@@ -38,26 +38,20 @@ db.tasks.insertMany([
     description: "Create a page for filtering using groups",
     status: "unfinished",
     group: "",
-    assignedTo: {
-      $oid: "6550da34a2e08ab6e5f80147",
-    },
+    assignedTo: ObjectId("6550da34a2e08ab6e5f80147"),
   },
   {
     title: "Create Home Page in Figma",
     description: "Start home page prototype in Figma",
     status: "unfinished",
     group: "",
-    assignedTo: {
-      $oid: "6550da34a2e08ab6e5f80147",
-    },
+    assignedTo: ObjectId("6550da34a2e08ab6e5f80147"),
   },
   {
     title: "Meeting w/ Lorens",
     description: "Discuss about app Google Meet Link: gugle.kom",
     status: "finished",
     group: "",
-    assignedTo: {
-      $oid: "6550da34a2e08ab6e5f80147",
-    },
+    assignedTo: ObjectId("6550da34a2e08ab6e5f80147"),
   },
 ]);
